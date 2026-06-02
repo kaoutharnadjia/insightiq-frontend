6f3a34f828517d71c64c623863d46f2449409cb5
unknown <barkisaberziad@gmail.com>
2026-06-02 19:32:31 +0200

    v

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,10 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true
+  }
 })
